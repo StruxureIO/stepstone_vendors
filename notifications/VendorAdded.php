9d62401c3d527aa89539759b194078b54709a290
AleksandrSuvorov <suvorov@site.pro>
2021-09-19 18:20:36 +0300

    Notification user type class VendorAdded. Sent test e-mail message

--- notifications/VendorAdded.php
+++ notifications/VendorAdded.php
@@ -3,16 +3,20 @@
 namespace humhub\modules\stepstone_vendors\notifications;
 
 use humhub\modules\notification\components\BaseNotification;
+use Yii;
+use yii\bootstrap\Html;
 
 class VendorAdded extends BaseNotification
 {
     public $moduleId = 'stepstone_vendors';
-    public $requireOriginator = false;
+//    public $requireOriginator = false;
     public $requireSource = false;
 
     public function html()
     {
-        return '<h1>TestedMailViewNotificationHTML 111</h1>';
+        return Yii::t('UserModule.notification', '{displayName} is now following you.', [
+            'displayName' => Html::tag('strong', Html::encode($this->originator->displayName)),
+        ]);
     }
 
     public function text()
